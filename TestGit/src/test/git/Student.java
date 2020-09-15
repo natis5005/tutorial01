ebe102c2e4b529618afb60a6629a1cd7b2aa06ae
Natalia Tabet <natalia.tabetagredo@mail.mcgill.ca>
2020-09-14 21:47:54 -0400

    name

--- TestGit/src/test/git/Student.java
+++ TestGit/src/test/git/Student.java
@@ -1,5 +1,21 @@
 package test.git;
 
 public class Student {
+	String name;
 
+	
+	public Student(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
 }
